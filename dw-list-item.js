dcbfe758597744587c00948ad2224d5d709328be
Trupti Baldaniya <baldaniyatrupti@gmail.com>
2019-12-10 17:10:57 +0530

    Code review changes

--- dw-list-item.js
+++ dw-list-item.js
@@ -19,7 +19,7 @@ import { alignment } from '@dreamworld/flex-layout/flex-layout-alignment';
 import { Typography } from '@dreamworld/material-styles/typography';
 import { factors } from '@dreamworld/flex-layout/flex-layout-factors';
 
-export class DwListItem extends LitElement { 
+export class DwListItem extends LitElement {
   static get styles() {
     return [
       flexLayout,
@@ -27,86 +27,197 @@ export class DwListItem extends LitElement {
       Typography,
       factors,
       css`
-        :host {
-          height: 48px;
-          padding: 0px 16px;
-          cursor: pointer;
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          position: relative;
-          transition: background-color 75ms linear;
-        }
-
-        :host(:hover){
-          background-color: var(--dw-list-item-hover-background, rgba(0,0,0,0.04));
-        }
-
-        :host([active]) {
-          background-color: var(--dw-list-item-active-background, rgba(0,0,0,0.08));
-        }
-
-        :host([selected]) {
-          background-color: var(--dw-list-item-selected-background);
-        }
-
-        :host([active][selected]){
-          background-color: var(--dw-list-item-selected-focused-background);
-        }
-        
-        :host([dense]) {
-          height: 36px;
-        }
-
-        :host([disabled]) .title1,
-        :host([disabled]) .title2 {
-          color: var(--dw-list-item-disabled-background, rgba(0,0,0,0.38));
-        }
-
-        :host([disabled]) {
-          pointer-events: none;
-        }
-
-        :host([selected]) .title-container .title1,
-        :host([selected]) .title-container .title2 {
-          font-weight: bold;
-        }
-
-        :host([twoLine]) {
-          height: 72px;
-        }
-
-        .leading-icon {
-          margin-right: 32px;
-        }
-
-        .title-container {
-          overflow: hidden;
-        }
-
-        .title-container .title2 {
-          color: var(--mdc-theme-text-secondary, rgba(0,0,0,0.54));
-        }
-
-        .title-container .title1 {
-          color: var(--mdc-theme-text-primary, rgba(0,0,0,0.87));
-        }
-
-        .title-container .title1.ellipsis,
-        .title-container .title2.ellipsis {
+        .ellipses{
           text-overflow: ellipsis;
           white-space: nowrap;
           overflow: hidden;
         }
 
-        dw-ripple {
-          position: absolute !important;
+        .mdc-list-item {
+          height: 48px;
+          position: relative;
+          justify-content: flex-start;
+          padding: 0 16px;
           overflow: hidden;
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
+          outline: none;
         }
+
+        .mdc-list-item__graphic {
+          background-color: transparent;
+          color: var(--dw-icon-color, rgba(0, 0, 0, 0.38));
+          margin-left: 0;
+          margin-right: 32px;
+          /* width: 24px;
+          height: 24px; */
+          flex-shrink: 0;
+          fill: currentColor;
+        }
+
+        .mdc-list-item--selected,
+        .mdc-list-item--activated,
+        .mdc-list-item--selected .mdc-list-item__graphic,
+        .mdc-list-item--activated .mdc-list-item__graphic {
+          color: var(--mdc-theme-primary, #6200ee);
+        }
+
+        .mdc-list-item--disabled .mdc-list-item__text {
+          opacity: 0.38;
+          color: var(--mdc-theme-text-disabled, #000);
+        }
+
+        .mdc-list-item__primary-text {
+          display: block;
+          margin-top: 0;
+          line-height: normal;
+          display: block;
+        }
+
+        .mdc-list--dense .mdc-list-item__primary-text {
+          display: block;
+          margin-top: 0;
+          line-height: normal;
+          margin-bottom: -20px;
+        }
+
+        .mdc-list-item__secondary-text {
+          color: var(--mdc-theme-text-secondary, rgba(0, 0, 0, 0.54));
+          font-family: Roboto, sans-serif;
+          -moz-osx-font-smoothing: grayscale;
+          -webkit-font-smoothing: antialiased;
+          font-size: 0.875rem;
+          line-height: 1.25rem;
+          font-weight: 400;
+          letter-spacing: 0.0178571429em;
+          text-decoration: inherit;
+          text-transform: inherit;
+          display: block;
+          margin-top: 0;
+          line-height: normal;
+          display: block;
+        }
+
+        .mdc-list--dense .mdc-list-item__secondary-text {
+          display: block;
+          margin-top: 0;
+          line-height: normal;
+          font-size: inherit;
+        }
+
+        .mdc-list--dense .mdc-list-item {
+          height: 40px;
+        }
+
+        .mdc-list--dense .mdc-list-item__graphic {
+          margin-left: 0;
+          margin-right: 36px;
+          width: 20px;
+          height: 20px;
+        }
+
+
+        .mdc-list--avatar-list .mdc-list-item {
+          height: 56px;
+        }
+
+        .mdc-list--avatar-list .mdc-list-item__graphic {
+          margin-left: 0;
+          margin-right: 16px;
+          width: 40px;
+          height: 40px;
+          border-radius: 50%;
+        }
+
+        .mdc-list--two-line .mdc-list-item__text {
+          align-self: flex-start;
+        }
+
+        .mdc-list--two-line .mdc-list-item {
+          height: 72px;
+        }
+
+        .mdc-list--two-line.mdc-list--dense .mdc-list-item,
+        .mdc-list--avatar-list.mdc-list--dense .mdc-list-item {
+          height: 60px;
+        }
+
+        .mdc-list--avatar-list.mdc-list--dense .mdc-list-item__graphic {
+          margin-left: 0;
+          margin-right: 20px;
+          width: 36px;
+          height: 36px;
+        }
+
+
+        :not(.mdc-list-item--disabled).mdc-list-item::before{
+          position: absolute;
+          border-radius: 50%;
+          opacity: 0;
+          pointer-events: none;
+          content: "";
+          top: calc(50% - 100%);
+          left: calc(50% - 100%);
+          width: 200%;
+          height: 200%;
+          background-color: #000;
+        }
+
+        :not(.mdc-list-item--disabled).mdc-list-item::before {
+          transition: opacity 15ms linear, background-color 15ms linear;
+          z-index: 1;
+        }
+
+        :not(.mdc-list-item--disabled).mdc-list-item:hover::before {
+          opacity: 0.04;
+        }
+
+        :not(.mdc-list-item--disabled).mdc-list-item--activated{
+          --mdc-theme-on-surface: red;
+
+        }
+
+        :not(.mdc-list-item--disabled).mdc-list-item--activated::before {
+          opacity: 0.12;
+        }
+
+
+        :not(.mdc-list-item--disabled).mdc-list-item--activated::before{
+          background-color: var(--mdc-theme-primary, #6200ee);
+        }
+
+        :not(.mdc-list-item--disabled).mdc-list-item--activated:hover::before {
+          opacity: 0.16;
+        }
+
+        :not(.mdc-list-item--disabled).mdc-list-item--selected::before {
+          opacity: 0.08;
+        }
+
+        :not(.mdc-list-item--disabled).mdc-list-item--selected::before{
+          background-color: var(--mdc-theme-primary, #6200ee);
+        }
+
+        :not(.mdc-list-item--disabled).mdc-list-item--selected:hover::before {
+          opacity: 0.12;
+        }
+
+        .mdc-list-item--disabled::before{
+          position: absolute;
+          border-radius: 50%;
+          opacity: 0;
+          pointer-events: none;
+          content: "";
+          top: calc(50% - 100%);
+          left: calc(50% - 100%);
+          width: 200%;
+          height: 200%;
+          background-color: #000;
+        }
+
+        .mdc-list-item--disabled::before {
+          transition: opacity 15ms linear, background-color 15ms linear;
+          z-index: 1;
+        }
+
       `
     ];
   }
@@ -164,25 +275,28 @@ export class DwListItem extends LitElement {
 
   render() {
     return html`
+    <div class="mdc-list-item layout horizontal center" tabindex="0">
       <dw-ripple></dw-ripple>
-      ${this.leadingIcon ? this._leadingIconTemplate() : html``}
-      <div class="layout vertical title-container flex">
-        <div class="title1 ellipsis body1">${this.title1}</div>
-        ${this.title2 && this.twoLine ? html`<div class="title2 body2 ellipsis">${this.title2}</div>` : ''}
-      </div>
-      ${this.trailingIcon ? this._trailingIconTemplate() : html``}
+      ${this.leadingIcon ? this._leadingIconTemplate() : ''}
+      <span class="mdc-list-item__text layout vertical flex ellipses">
+        <span class="mdc-list-item__primary-text ellipses">Two-line item</span>
+        ${this.title2 && this.twoLine ? html`<span class="mdc-list-item__secondary-text ellipses">Secondary text</span>` : ''}
+      </span>
+      ${this.trailingIcon ? this._trailingIconTemplate() : ''}
+    </div>
+
     `
   }
 
   _leadingIconTemplate(){
     return html`
-      <dw-icon class="leading-icon" ?disabled="${this.disabled}" name="${this.leadingIcon}"></dw-icon>
+      <dw-icon class="leading-icon mdc-list-item__graphic" ?disabled="${this.disabled}" name="${this.leadingIcon}"></dw-icon>
     `
   }
 
   _trailingIconTemplate(){
     return html`
-      <dw-icon-button ?disabled="${this.disabled}" .icon="${this.trailingIcon}"></dw-icon-button>
+      <dw-icon-button class="mdc-list-item__graphic" ?disabled="${this.disabled}" .icon="${this.trailingIcon}"></dw-icon-button>
     `
   }
 
