88f71b3b475c02492329a50919988ad58585616a
devangdreamworld <devang@hisab.co>
2022-07-18 13:42:31 +0530

    feat: add new property to show ripples on the selected item

--- dw-list-item.js
+++ dw-list-item.js
@@ -282,7 +282,13 @@ export class DwListItem extends LitElement {
        * Input property.
        * use to set placement of tooltip.
        */
-      tooltipPlacement : { type: String }
+      tooltipPlacement : { type: String },
+
+      /**
+       * Input property.
+       * Set it to true to show ripples on the selected item.
+       */
+      showSelectedRipple : { type: Boolean },
     };
   }
 
@@ -339,7 +345,7 @@ export class DwListItem extends LitElement {
   render() {
     return html`
 
-      ${this.disabled ? '' : html`<dw-ripple .primary=${this.selected}></dw-ripple>`}
+      ${this.disabled ? '' : html`<dw-ripple .primary=${this.showSelectedRipple}></dw-ripple>`}
 
       <!-- Leading icon -->
       ${this.hasLeadingIcon ? this._leadingIconTemplate : ''}
